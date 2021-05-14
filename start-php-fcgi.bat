772cc5d8a3ddc3421d7061e2bb878b0d1ebf71d4
Max Base <MaxBaseCode@Gmail.Com>
2021-05-15 04:04:41 +0430

    Unlimit infinity loop, restart if this failed!

--- start-php-fcgi.bat
+++ start-php-fcgi.bat
@@ -2,4 +2,6 @@
 ECHO Starting PHP FastCGI...
 set PATH=C:\php;%PATH%
 
-C:\php\php-cgi.exe -b 127.0.0.1:9123 -c C:\php\php.ini
+:start
+	C:\php\php-cgi.exe -b 127.0.0.1:9123 -c C:\php\php.ini
+	goto start
